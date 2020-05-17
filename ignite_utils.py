5eff3dcba780a7cf77e737f032dc69fceb1c628e
Blaine Rothrock <blaine.p.rothrock@gmail.com>
2020-05-17 09:13:16 -0500

    loss & perplexity reporting, tensorboard, torch ignite

---
+++ ignite_utils.py
@@ -0,0 +1,46 @@
+from ignite.engine import Engine
+import torch
+
+
+def create_engine(model, optimizer, loss_fn, device):
+    model.to(device)
+
+    def _update(engine, batch):
+        model.train()
+        model.zero_grad()
+
+        seq = batch.text.to(device)
+        target = batch.target.view(-1).to(device)
+
+        output = model(seq)
+
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+
+        return loss.item()
+
+    return Engine(_update)
+
+
+def create_evaluator(model, metrics=None, device=None):
+    metrics = metrics or {}
+
+    if device:
+        model.to(device)
+
+    def _inference(engine, batch):
+        model.eval()
+        with torch.no_grad():
+            seq = batch.text.to(device)
+            target = batch.target.view(-1).to(device)
+            pred = model(seq)
+
+            return pred, target
+
+    engine = Engine(_inference)
+
+    for name, metric in metrics.items():
+        metric.attach(engine, name)
+
+    return engine
--- train.py
+++ train.py
@@ -1,11 +1,14 @@
 import os, gin
 import numpy as np
-import torch
-import torchtext
+import torch, torchtext
+from torch.utils.tensorboard import SummaryWriter
+from ignite.engine import create_supervised_evaluator, Events
+from ignite_utils import create_engine, create_evaluator
+from ignite.metrics import Loss
 from model import LM
 
 @gin.configurable()
-def train(max_epochs, batch_size, learning_rate, context_window):
+def train(max_epochs, batch_size, learning_rate, momentum, context_window):
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -16,32 +19,51 @@ def train(max_epochs, batch_size, learning_rate, context_window):
 
     model = LM(vocab_size=vocab_size)
     loss_fn = torch.nn.NLLLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
-    model = model.to(device)
-    model.train()
+    trainer = create_engine(model, optimizer, loss_fn, device)
+    evaluator = create_evaluator(model, metrics={'nll': Loss(loss_fn)}, device=device)
 
-    for epoch in range(max_epochs):
-        running_loss = []
-        iter_count = 0
-        for item in train_iter:
-            seq = item.text.to(device)
-            target = item.target.view(-1).to(device)
+    writer = SummaryWriter()
 
-            model.zero_grad()
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_iter_loss(engine):
+        writer.add_scalar('Iter/train_loss', engine.state.output, engine.state.iteration)
 
-            output = model(seq)
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_train_loss(engine):
+        loss = engine.state.output
+        epoch = engine.state.epoch
+        writer.add_scalar('Loss/train', loss, epoch)
+        writer.add_scalar('Perplexity/train', np.exp(loss), epoch)
+        print('----------------')
+        print('Epoch %i:' % epoch)
+        print('  - train loss: %.2f' % loss)
 
-            loss = loss_fn(output, target)
-            running_loss.append(loss.item())
-            iter_count += 1
-            loss.backward()
-            optimizer.step()
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_val_loss(engine):
+        epoch = engine.state.epoch
+        evaluator.run(val_iter)
+        metrics = evaluator.state.metrics
+        loss = metrics['nll']
+        perplexity = np.exp(loss)
+        writer.add_scalar('Loss/val', loss, epoch)
+        writer.add_scalar('Perplexity/val', perplexity, epoch)
+        print('  - val loss:   %.2f' % loss)
 
-        if epoch % 1 == 0:
-            print('Epoch: %i\n   - Loss: %.2f' % (epoch, np.mean(running_loss)))
-            running_loss = []
-            iter_count = 0
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_test_loss(engine):
+        epoch = engine.state.epoch
+        evaluator.run(test_iter)
+        metrics = evaluator.state.metrics
+        loss = metrics['nll']
+        perplexity = np.exp(loss)
+        writer.add_scalar('Loss/test', loss, epoch)
+        writer.add_scalar('Perplexity/test', perplexity, epoch)
+        print('  - test loss:  %.2f' % loss)
+
+    trainer.run(train_iter, max_epochs=max_epochs)
+
 
 if __name__ == '__main__':
     gin.parse_config_file('config.gin')
