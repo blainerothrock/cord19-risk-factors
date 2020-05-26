365180faa929a0ac4dadb91a089bf6e8c9b3e054
Grant Gasser <glgasser@gmail.com>
2020-05-26 13:52:46 -0700

    Update docstrings.

--- dataset.py
+++ dataset.py
@@ -16,20 +16,20 @@ class Cord19(LanguageModelingDataset):
     def splits(cls, text_field, root='.data', train='train_tokens.txt',
                validation='validation_tokens.txt', test='test_tokens.txt',
                **kwargs):
-        """Create dataset objects for splits of the WikiText-2 dataset.
+        """Create dataset objects for splits of the Cord19 dataset.
 
         This is the most flexible way to use the dataset.
 
         Arguments:
             text_field: The field that will be used for text data.
             root: The root directory that the dataset's zip archive will be
-                expanded into; therefore the directory in whose wikitext-2
+                expanded into; therefore the directory in whose cord19
                 subdirectory the data files will be stored.
-            train: The filename of the train data. Default: 'wiki.train.tokens'.
+            train: The filename of the train data. Default: 'train_tokens.txt'.
             validation: The filename of the validation data, or None to not
-                load the validation set. Default: 'wiki.valid.tokens'.
+                load the validation set. Default: 'validation_tokens.txt'.
             test: The filename of the test data, or None to not load the test
-                set. Default: 'wiki.test.tokens'.
+                set. Default: 'test_tokens.txt'.
         """
         return super(Cord19, cls).splits(
             root=root, train=train, validation=validation, test=test,
@@ -38,7 +38,7 @@ class Cord19(LanguageModelingDataset):
     @classmethod
     def iters(cls, batch_size=32, bptt_len=35, device=0, root='cord19_data',
               vectors=None, **kwargs):
-        """Create iterator objects for splits of the WikiText-2 dataset.
+        """Create iterator objects for splits of the Cord19 dataset.
 
         This is the simplest way to use the dataset, and assumes common
         defaults for field, vocabulary, and iterator parameters.
@@ -49,7 +49,7 @@ class Cord19(LanguageModelingDataset):
             device: Device to create batches on. Use -1 for CPU and None for
                 the currently active GPU device.
             root: The root directory that the dataset's zip archive will be
-                expanded into; therefore the directory in whose wikitext-2
+                expanded into; therefore the directory in whose cord19
                 subdirectory the data files will be stored.
             wv_dir, wv_type, wv_dim: Passed to the Vocab constructor for the
                 text field. The word vectors are accessible as
